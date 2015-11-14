5688afb6620b125efe8f194324669f3d6e832852
Richard Zetterberg <richard.zetterberg@googlemail.com>
2015-11-14 10:32:46 +0100

    Fixes compile error, adds time conversion

--- lib/Database/OrgMode/Marshall.hs
+++ lib/Database/OrgMode/Marshall.hs
@@ -8,19 +8,31 @@ Currently only contains functionlity to convert into database types.
 module Database.OrgMode.Marshall where
 
 import           Data.OrgMode.Parse.Types
-import           Data.Time.Clock (UTCTime(..))
+import           Data.Time.Clock (UTCTime(..), DiffTime)
 import           Data.Time.Clock (diffUTCTime)
 import qualified Data.Time.Calendar as T
+import qualified Data.Time.Calendar.WeekDate as T
 import qualified Data.Time.Clock as T
 
 import           Database.OrgMode.Import
 import qualified Database.OrgMode.Model as Db
 
 --------------------------------------------------------------------------------
+-- * Types
 
 type Clock = (Maybe Timestamp, Maybe Duration)
 
 --------------------------------------------------------------------------------
+-- * Constants
+
+startDayTime :: DiffTime
+startDayTime = T.secondsToDiffTime 0
+
+endDayTime :: DiffTime
+endDayTime = T.secondsToDiffTime 86400
+
+--------------------------------------------------------------------------------
+-- * Orgmode-parse related marshalling
 
 documentToDb :: Text -> Document -> Db.Document
 documentToDb name Document{..} = Db.Document name documentText
@@ -52,6 +64,9 @@ clockToDb secId (Just tstamp, _) = Just clock
         Nothing  -> 0
         Just end -> round $ diffUTCTime end start
 
+--------------------------------------------------------------------------------
+-- * Time marshalling
+
 {-|
 Helper that takes date information from a orgmode-attoparsec document and
 converts it to a 'UTCTime' that can be saved in the database.
@@ -65,3 +80,26 @@ dateTimeToUTC (DateTime cal _ clock _ _)
     calcSecs Nothing       = 0
     calcSecs (Just (h, m)) = ((h * 60) + m) * 60
     midSecs                = toInteger $ calcSecs clock
+
+{-|
+Takes a year and month and converts it to a pair of 'UTCTime' that represents
+the start of the month (first day, 00:00:00) and the end of the month
+(last day, 23:59:59).
+-}
+yearMonthToUTC :: Integer -> Int -> (UTCTime, UTCTime)
+yearMonthToUTC year month = (start, end)
+  where
+    start  = UTCTime (T.fromGregorian year month 1) startDayTime
+    end    = UTCTime (T.fromGregorian year month endDay) endDayTime
+    endDay = T.gregorianMonthLength year month
+
+{-|
+Takes a year and week and converts it to a pair of 'UTCTime' that represents
+the start of the week (first day, 00:00:00) and the end of the month
+(last day, 23:59:59).
+-}
+yearWeekToUTC :: Integer -> Int -> (UTCTime, UTCTime)
+yearWeekToUTC year week = (start, end)
+  where
+    start = UTCTime (T.fromWeekDate year week 1) startDayTime
+    end   = UTCTime (T.fromWeekDate year week 7) endDayTime
