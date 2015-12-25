b32f4a6bc674ffa8361805529a723c7d0b451857
Richard Zetterberg <richard.zetterberg@googlemail.com>
2015-12-25 14:40:41 +0100

    Adds benchmarks to clock table

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -4,9 +4,11 @@ import           Criterion.Main (defaultMain)
 import           Prelude
 
 import qualified Query.ExportBench as Export
+import qualified Query.ClockTableBench as ClockTable
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = defaultMain [ Export.benches
+                   , ClockTable.benches
                    ]
---
+++ bench/Query/ClockTableBench.hs
@@ -0,0 +1,51 @@
+module Query.ClockTableBench (benches) where
+
+import           BenchImport
+import           Criterion.Types
+import           Data.Attoparsec.Text (parseOnly)
+import           Data.Default
+import qualified Data.OrgMode.Parse.Attoparsec.Document as OrgParse
+import qualified Data.Text as T
+
+import qualified Database.OrgMode.Import as OrgDb
+import qualified Database.OrgMode.Query.ClockTable as ClockTable
+
+--------------------------------------------------------------------------------
+
+benches :: Benchmark
+benches = bgroup "ClockTable" [ benchSmallDoc
+                              , benchMediumDoc
+                              , benchLargeDoc
+                              , benchVeryLargeDoc
+                              ]
+
+benchSmallDoc :: Benchmark
+benchSmallDoc = bench "small doc" $ whnfIO $ do
+    mkClockTableBench "1_section_2_subs.org"
+
+benchMediumDoc :: Benchmark
+benchMediumDoc = bench "medium doc" $ whnfIO $ do
+    mkClockTableBench "4_sections_3_tags_2_clocks.org"
+
+benchLargeDoc :: Benchmark
+benchLargeDoc = bench "large doc" $ whnfIO $ do
+    mkClockTableBench "45_sections_multi_clocks_tree.org"
+
+benchVeryLargeDoc :: Benchmark
+benchVeryLargeDoc = bench "very large doc" $ whnfIO $ do
+    mkClockTableBench "90_sections_multi_clocks_tree.org"
+
+--------------------------------------------------------------------------------
+-- * Helpers
+
+allowedTags :: [Text]
+allowedTags = ["DONE", "TODO"]
+
+mkClockTableBench :: FilePath -> IO ()
+mkClockTableBench fname = getExample fname >>=
+    (go . parseOnly (OrgParse.parseDocument allowedTags))
+  where
+    go (Left err)  = error $ "Example could not be parsed: " ++ err
+    go (Right doc) = void $ runDb $ do
+        void $ OrgDb.importDocument (T.pack fname) doc
+        ClockTable.getTable def
